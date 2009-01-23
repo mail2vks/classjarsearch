e5109cda30d3175eed1adbec89074892737a8cbb
singh.kr.vivek <singh.kr.vivek@386545ca-e871-11dd-9ba5-89a75009fd5d>
2009-01-23 02:37:54 +0000

    Updated files using checkstyle and formatter. Need to do more work

--- Java_Jar_Search/src/com/vivek/file/DirectoryReader.java
+++ Java_Jar_Search/src/com/vivek/file/DirectoryReader.java
@@ -7,107 +7,133 @@ import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-//import org.apache.log4j.Logger;
+/**
+ * @author singh.kr.vivek
+ * 
+ */
+public class DirectoryReader
+{
 
-public class DirectoryReader {
+    /**
+     * Constant to store number of milliseconds in a second
+     */
+    private static final int MILLISECONDS = 1000;
 
-	//private static Logger logger = Logger.getLogger(DirectoryReader.class);
+    public static void main(String args[])
+    {
+        if (args.length < 2)
+        {
+            System.out.println("Usage:"
+                               + " java -jar jarSearch.jar"
+                               + " <Location to Search>"
+                               + " <Class File Name>");
+        }
+        else
+        {
+            DirectoryReader reader = new DirectoryReader();
+            reader.search(new JarFilter(), args[1], args[0]);
+        }
+    }
 
-	public static void main(String args[]) {
-		if (args.length < 2) {
-			System.out
-					.println("Usage: java -jar jarSearch.jar <Location to Search> <Class File Name>");
-		} else {
-			DirectoryReader.search(new JarFilter(), args[1], args[0]);
-		}
-	}
+    private void search(FilenameFilter filter, String searchString,
+        String directory)
+    {
 
-	public static void search(FilenameFilter filter, String searchString,
-			String directory) {
+        File file = new File(directory);
 
-		File file = new File(directory);
+        long startTime = System.currentTimeMillis();
 
-		long startTime = System.currentTimeMillis();
+        parse(file, filter, searchString);
 
-		parse(file, filter, searchString);
+        long endTime = System.currentTimeMillis();
 
-		long endTime = System.currentTimeMillis();
+        long timeTaken = (endTime - startTime) / MILLISECONDS;
+        System.out.println("Time taken " + timeTaken + "s");
+    }
 
-		long timeTaken = (endTime - startTime) / 1000;
-		System.out.println("Time taken " + timeTaken + "s");
-		//logger.info("Time taken " + timeTaken + "s");
+    private void parse(File f, FilenameFilter filter,
+        String searchString)
+    {
 
-	}
+        if (f.isFile())
+        {
+            searchClassInJar(f, searchString);
 
-	private static void parse(File f, FilenameFilter filter, String searchString) {
+        }
+        else if (f.isDirectory() && !f.isHidden())
+        {
 
-		if (f.isFile()) {
-			//logger.debug("Now Searching File : " + f.getPath());
-			searchClassInJar(f, searchString);
+            getContent(f, filter, searchString);
+        }
 
-		} else if (f.isDirectory() && f.isHidden() == false) {
-			//logger.debug("Now Parsing Directory : " + f.getPath());
-			getContent(f, filter, searchString);
-		}
+    }
 
-	}
+    private void getContent(File f, FilenameFilter filter,
+        String searchString)
+    {
 
-	private static void getContent(File f, FilenameFilter filter,
-			String searchString) {
+        File[] fileArr = f.listFiles(filter);
 
-		File[] fileArr = f.listFiles(filter);
+        if (fileArr != null)
+        {
+            for (File fi : fileArr)
+            {
+                if (fi.isFile())
+                {
+                    searchClassInJar(fi, searchString);
+                }
 
-		if (fileArr != null) {
-			for (File fi : fileArr) {
-				if (fi.isFile()) {
-					searchClassInJar(fi, searchString);
-					//logger.debug(fi.getPath());
-				}
+            }
+        }
 
-			}
-		}
+        File[] dirArr = f.listFiles();
 
-		File[] dirArr = f.listFiles();
+        if (dirArr != null)
+        {
+            for (File fi : dirArr)
+            {
+                if (fi.isDirectory())
+                {
+                    parse(fi, filter, searchString);
+                }
+            }
+        }
 
-		if (dirArr != null) {
-			for (File fi : dirArr) {
-				if (fi.isDirectory())
-					parse(fi, filter, searchString);
-			}
-		}
+    }
 
-	}
+    private void searchClassInJar(File f, String searchString)
+    {
 
-	private static void searchClassInJar(File f, String searchString) {
+        String fileName = null;
+        try
+        {
+            if (f.canRead())
+            {
 
-		String fileName = null;
-		//logger
-				//.debug("Searching for \"" + searchString + "\" in "
-						//+ f.getPath());
-		try {
-			if (f.canRead()) {
+                JarFile jarFile = new JarFile(f);
 
-				JarFile jarFile = new JarFile(f);
+                Enumeration<JarEntry> entries = jarFile.entries();
 
-				Enumeration<JarEntry> entries = jarFile.entries();
+                while (entries.hasMoreElements())
+                {
+                    fileName = entries.nextElement().getName();
+                    if (fileName.indexOf(searchString) != -1)
+                    {
+                        System.out.println("Found "
+                                           + searchString
+                                           + " as "
+                                           + fileName
+                                           + " in "
+                                           + f.getPath());
+                    }
+                }
 
-				while (entries.hasMoreElements()) {
-					fileName = entries.nextElement().getName();
-					//logger.debug(fileName);
-					if (fileName.indexOf(searchString) != -1) {
-						System.out.println("Found " + searchString + " as "
-								+ fileName + " in " + f.getPath());
-						//logger.info("Found " + searchString + " as " + fileName
-								//+ " in " + f.getPath());
-					}
-				}
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			//logger.error("Error Occurred when parsing " + f.getPath(), e
-					//.getCause());
-		}
-
-	}
+    }
 }
